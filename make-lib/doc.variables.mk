c1ec1ce88bd18241129f20ec750d469de3ab8a73
Ron Schmitt <11559541+ron2015schmitt@users.noreply.github.com>
2022-09-09 14:12:11 -0400

    fixed doc makefile typos

--- make-lib/doc.variables.mk
+++ make-lib/doc.variables.mk
@@ -17,11 +17,11 @@ MAKEFILE_REL := $(subst $(DIR_MATHQ)/,,$(MAKEFILE))
 CREATE_DOC_BRANCH := $(DIR_MATHQ)/scripts/doc_create_subnodes.py
 CREATE_DOC_LEAF := $(DIR_MATHQ)/scripts/doc_create_leaf.py
 
-# CHAPTERS must be defined in the "incuder" file
-CHAPExpressionR_NODE_FILES = $(addsuffix /node.json,$(CHAPTERS))
-CHAPExpressionR_BRANCHES = $(subst /,,$(MAKE_SUBDIRS))
-CHAPExpressionR_LEAVES = $(filter-out $(CHAPExpressionR_BRANCHES),$(CHAPTERS))
+# CHAPTERS must be defined in the specific makefile
+CHAPTER_NODES = $(addsuffix /node.json,$(CHAPTERS))
+CHAPTER_BRANCHES = $(subst /,,$(MAKE_SUBDIRS))
+CHAPTER_LEAVES = $(filter-out $(CHAPTER_BRANCHES),$(CHAPTERS))
 
-LEAF_READMES = $(addsuffix /README.md,$(CHAPExpressionR_LEAVES))
+LEAF_READMES = $(addsuffix /README.md,$(CHAPTER_LEAVES))
 
 
